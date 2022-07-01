ed51aa333c7edf25c33b2366387eea361baf0986
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2022-07-01 08:25:33 +0700

    fix(SourceProxyRoutes): 🐛 remove custom scrapers

--- src/routes.ts
+++ src/routes.ts
@@ -4,29 +4,27 @@ import express, { Request, Response } from 'express';
 
 import scrapers from './scrapers';
 
-import videoUploadController from './controllers/videoUploadController';
-import imageSourceController from './controllers/imageSourceController';
-import videoSourceController from './controllers/videoSourceController';
 import animeEpisodeController from './controllers/animeEpisodeController';
-import videoStatusController from './controllers/videoStatusController';
+import fileUploadController from './controllers/fileUploadController';
+import imageSourceController from './controllers/imageSourceController';
 import videoRemoteStatusController from './controllers/videoRemoteStatusController';
 import videoRemoteUploadController from './controllers/videoRemoteUploadController';
-import fileUploadController from './controllers/fileUploadController';
+import videoSourceController from './controllers/videoSourceController';
+import videoStatusController from './controllers/videoStatusController';
+import videoUploadController from './controllers/videoUploadController';
 
 import auth from './middlewares/auth';
 import checkUploadPermission from './middlewares/checkUploadPermission';
 import validate from './middlewares/validate';
 
-import { videoRemoteUploadValidation } from './validations/videoRemoteUploadValidation';
-import { videoUploadValidation } from './validations/videoUploadValidation';
-import { uploadEpisodeValidation } from './validations/uploadEpisodeValidation';
-import { fileUploadValidation } from './validations/fileUploadValidation';
-import { videoStatusValidation } from './validations/videoStatusValidation';
-import { videoRemoteStatusValidation } from './validations/videoRemoteStatusValidation';
 import fileProxyController from './controllers/fileProxyController';
 import { fileProxyValidation } from './validations/fileProxyValidation';
-import supabase from './lib/supabase';
-import { Source } from './types/data';
+import { fileUploadValidation } from './validations/fileUploadValidation';
+import { uploadEpisodeValidation } from './validations/uploadEpisodeValidation';
+import { videoRemoteStatusValidation } from './validations/videoRemoteStatusValidation';
+import { videoRemoteUploadValidation } from './validations/videoRemoteUploadValidation';
+import { videoStatusValidation } from './validations/videoStatusValidation';
+import { videoUploadValidation } from './validations/videoUploadValidation';
 
 const cache = apicache.middleware;
 
@@ -42,24 +40,14 @@ router.get('/', (_, res) => {
 router.get('/proxy/sources', async (_, res) => {
   const allScrapers = { ...scrapers.anime, ...scrapers.manga };
 
-  const { data: customSources = [] } = await supabase
-    .from<Source>('kaguya_sources')
-    .select('id')
-    .eq('isCustomSource', true);
-
   const proxySources = Object.entries(allScrapers).map(([key, value]) => ({
     headers: value.proxy.headers,
     id: key,
   }));
 
-  const customProxySources = customSources.map((source) => ({
-    headers: allScrapers.custom.proxy.headers,
-    id: source.id,
-  }));
-
   res.json({
     success: true,
-    sources: [...proxySources, ...customProxySources],
+    sources: proxySources,
   });
 });
 
